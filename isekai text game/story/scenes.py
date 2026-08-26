5b8f05fcd91a2e39573d3049d1304d6021aa1b61
LeoVal7 <leonardo.felix.rock@gmail.com>
2026-08-26 12:35:30 +0200

    Add files via upload

--- isekai text game/story/scenes.py
+++ isekai text game/story/scenes.py
@@ -8,7 +8,23 @@ def risveglio(game):
     narra("Il sole è alto nel cielo ")
     pausa()
     narra("Ti guardi attorno e vedi solo una foresta che circonda il prato")
-
+    scelta = scegli(
+        "Osserva il prato",
+        "Guardare verso la foresta",
+        "Prova a ricordare come sei arrivato qui"
+    )
+    if scelta == 1:
+        narra("Il prato è pieno di fiori rossi e il vento li fa ondeggiare dolcemente")
+        pausa()
+        return "risveglio"
+    elif scelta == 2:
+        narra("La foresta sembra oscura e minacciosa, ma c'è un sentiero che porta verso di essa")
+        pausa()
+        return "risveglio"
+    elif scelta == 3:
+        narra("Non ricordi nulla, solo un senso di vuoto e confusione")
+        pausa()
+        return "risveglio"
 SCENE = {
     "risveglio": risveglio
 }
